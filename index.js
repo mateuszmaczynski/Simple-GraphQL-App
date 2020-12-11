658c6fa4d312d6739dc09a7166452f6371e048eb
Mateusz <mateusz@Mac-mini-Mateusz.local>
2020-12-11 22:01:02 +0100

    Create a simple GraphQL server

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log('Hello world')
+const { ApolloServer } = require("apollo-server");
+
+const typeDefs = `
+  type Query {
+    greeting: String
+  }
+`;
+
+const server = new ApolloServer({ typeDefs });
+
+server.listen({ port: 4000}).then((result) => console.log(result.url));
